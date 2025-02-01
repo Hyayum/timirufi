87b04d00770c4fa2b1ce012a7d950f4adc2eb9ff
timireno <hinomefi@gmail.com>
2025-02-02 03:43:33 +0900

    ヘッダ整備

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head />
+      <Head>
+        <title>てぃみ*れの / みるふぃ</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
